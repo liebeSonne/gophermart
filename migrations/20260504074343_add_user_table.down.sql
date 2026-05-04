2e6331da3cc1b1253288558b3b5c999b406727d6
liebeSonne <>
2026-05-04 10:50:41 +0300

    v1 - добавил миграции добавления таблицы пользователей

---
+++ migrations/20260504074343_add_user_table.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS user_login_idx;
+DROP TABLE IF EXISTS user;
---
+++ migrations/20260504074343_add_user_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user
+(
+    id UUID NOT NULL PRIMARY KEY,
+    login      VARCHAR(255) NOT NULL,
+    passhash   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX user_login_idx ON user (login);
