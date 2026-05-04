bb7897946df84f8e93e0c008861efd86ff0786f7
liebeSonne <>
2026-05-04 11:08:57 +0300

    v1 - добавил репозиторий хранения пользователей

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.26.2
 require (
 	github.com/getkin/kin-openapi v0.135.0
 	github.com/go-chi/chi/v5 v5.2.5
+	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/jackc/pgx/v5 v5.9.2
 	github.com/sirupsen/logrus v1.9.4
 	github.com/stretchr/testify v1.11.1
 )
@@ -16,6 +18,9 @@ require (
 	github.com/dprotaso/go-yit v0.0.0-20220510233725-9ba8df137936 // indirect
 	github.com/go-openapi/jsonpointer v0.22.4 // indirect
 	github.com/go-openapi/swag/jsonname v0.25.4 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.9.1 // indirect
---
+++ internal/repository/user.go
@@ -0,0 +1,111 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/liebeSonne/gophermart/internal/model"
+)
+
+var ErrUserNotFound = errors.New("user not found")
+
+type UserRepository interface {
+	NextID() uuid.UUID
+	Store(ctx context.Context, user model.User) error
+	GetByID(ctx context.Context, userID uuid.UUID) (model.User, error)
+	FindByLogin(ctx context.Context, login string) (*model.User, error)
+}
+
+func NewUserRepository(
+	pool *pgxpool.Pool,
+) UserRepository {
+	return &userRepository{
+		pool: pool,
+	}
+}
+
+type userRepository struct {
+	pool *pgxpool.Pool
+}
+
+func (r *userRepository) NextID() uuid.UUID {
+	return uuid.New()
+}
+
+func (r *userRepository) Store(ctx context.Context, user model.User) error {
+	tx, err := r.pool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return fmt.Errorf("error on begin transaction: %w", err)
+	}
+
+	defer func() {
+		err = tx.Rollback(ctx)
+		if err != nil {
+			fmt.Printf("error on rollback transaction: %v\n", err)
+		}
+	}()
+
+	const sqlQuery = `
+		INSERT INTO user (id, login, passhash) VALUES ($1, $2, $3)
+	`
+
+	_, err = tx.Exec(ctx, sqlQuery, user.ID, user.Login, user.PassHash)
+	if err != nil {
+		return fmt.Errorf("error on insert user: %w", err)
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return fmt.Errorf("error on commit transaction: %w", err)
+	}
+
+	return nil
+}
+
+func (r *userRepository) GetByID(ctx context.Context, userID uuid.UUID) (model.User, error) {
+	const sqlQuery = `
+		SELECT id, login, passhash 
+		FROM user 
+		WHERE id = $1 
+		LIMIT 1
+	`
+
+	var user model.User
+	row := r.pool.QueryRow(ctx, sqlQuery, userID)
+	err := row.Scan(&user.ID, &user.Login, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return model.User{}, fmt.Errorf("user %s not found: %w", userID.String(), ErrUserNotFound)
+		}
+		return model.User{}, fmt.Errorf("error on scan row: %w", err)
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) FindByLogin(ctx context.Context, login string) (*model.User, error) {
+	const sqlQuery = `
+		SELECT id, login, passhash 
+		FROM user 
+		WHERE login = $1 
+		LIMIT 1
+	`
+
+	var user model.User
+	row := r.pool.QueryRow(ctx, sqlQuery, login)
+	err := row.Scan(&user.ID, &user.Login, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("error on scan row: %w", err)
+	}
+
+	return &user, nil
+}
