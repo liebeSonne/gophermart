34c0af45b82ab0b4cfefa8f322a61a639068c429
liebeSonne <>
2026-05-05 14:54:15 +0300

    v1 - реализовал метод аутентификации

--- internal/handler/server.go
+++ internal/handler/server.go
@@ -49,11 +49,45 @@ func (s *Server) WithdrawUserBalance(w http.ResponseWriter, r *http.Request) {
 	panic("implement me")
 }
 
+//nolint:dupl
 func (s *Server) LoginUser(w http.ResponseWriter, r *http.Request) {
-	// TODO implement me
-	_ = w
-	_ = r
-	panic("implement me")
+	ctx := r.Context()
+
+	var userCredentials server.UserCredentials
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&userCredentials)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	input := service.LoginUserInput{
+		Login:    userCredentials.Login,
+		Password: userCredentials.Password,
+	}
+
+	user, err := s.userService.Login(ctx, input)
+	if err != nil {
+		s.logger.WithError(err).Errorf("error creating user (login: %s)", input.Login)
+
+		if errors.Is(err, service.ErrNotValidUserLoginPassword) || errors.Is(err, service.ErrUserNotFound) {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = s.setUserAuthorization(w, r, user)
+	if err != nil {
+		s.logger.WithError(err).Errorf("error setting user (id: %s, login: %s) authorization", user.ID, user.Login)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func (s *Server) GetUserOrders(w http.ResponseWriter, r *http.Request) {
@@ -70,6 +104,7 @@ func (s *Server) UploadUserOrders(w http.ResponseWriter, r *http.Request) {
 	panic("implement me")
 }
 
+//nolint:dupl
 func (s *Server) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
