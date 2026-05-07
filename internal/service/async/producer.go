09dc0e1cc1056509219f7859ffbee72bca20bc70
liebeSonne <>
2026-05-07 17:53:24 +0300

    v1 - добавил реализацию поставщика метод планирования добавления значения; оставил общую реализацию поставщика для обработки http запросов и повторных попыток

---
+++ internal/service/async/producer.go
@@ -0,0 +1,106 @@
+package async
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type RequestProducer interface {
+	Produce() <-chan string
+	Add(value string)
+}
+
+type RetryProducer interface {
+	Produce() <-chan string
+	Schedule(value string, delay time.Duration) *time.Timer
+}
+
+type Producer interface {
+	Produce() <-chan string
+	Add(value string)
+	Schedule(value string, delay time.Duration) (cancel func() bool)
+}
+
+func NewProducer(
+	ctx context.Context,
+	name string,
+	channelSize uint,
+	logger *logrus.Logger,
+) Producer {
+	p := &producer{
+		ctx:    ctx,
+		name:   name,
+		ch:     make(chan string, channelSize),
+		logger: logger,
+	}
+
+	startTime := time.Now()
+	p.logger.Infof("'%s' producer started at %v", p.name, startTime)
+
+	go func() {
+		<-ctx.Done()
+		p.logger.Infof("'%s' producer closed: context closed", p.name)
+
+		p.mu.Lock()
+		close(p.ch)
+		p.closed = true
+		p.mu.Unlock()
+
+		p.logger.Infof("'%s' producer finished at %v (%v)", p.name, time.Now(), time.Since(startTime))
+	}()
+
+	return p
+}
+
+type producer struct {
+	ctx    context.Context
+	name   string
+	logger *logrus.Logger
+	ch     chan string
+	closed bool
+	mu     sync.RWMutex
+}
+
+func (p *producer) Produce() <-chan string {
+	return p.ch
+}
+
+func (p *producer) Add(value string) {
+	p.mu.RLock()
+	isClosed := p.closed
+	ch := p.ch
+	p.mu.RUnlock()
+
+	if isClosed || ch == nil {
+		return
+	}
+
+	select {
+	case ch <- value:
+		p.logger.Debugf("'%s' producer add value (%s)", p.name, value)
+	case <-p.ctx.Done():
+		p.logger.Debugf("'%s' producer context closed (%v) on add value (%v)", p.name, p.ctx.Err(), value)
+		return
+	}
+}
+
+func (p *producer) Schedule(value string, delay time.Duration) (cancel func() bool) {
+	p.logger.Debugf("'%s' producer run schedule add value (%s) delay (%v)", p.name, value, delay)
+
+	timer := time.AfterFunc(delay, func() {
+		select {
+		case <-p.ctx.Done():
+			return
+		default:
+			p.Add(value)
+		}
+	})
+
+	cancel = func() bool {
+		return timer.Stop()
+	}
+	return
+}
---
+++ internal/service/async/producer_test.go
@@ -0,0 +1,226 @@
+package async
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProducer_Add(t *testing.T) {
+	type on struct {
+		channelSize            uint
+		addValuesBeforeCtxDone []string
+		cancelCtx              bool
+		addValuesAfterCtxDone  []string
+	}
+	type want struct {
+		values []string
+	}
+	testCases := []struct {
+		name string
+		on   on
+		want want
+	}{
+		{
+			"ctx Done",
+			on{10, []string{"1", "2", "3"}, true, []string{"4", "5", "6"}},
+			want{[]string{"1", "2", "3"}},
+		},
+		{
+			"ctx not Done",
+			on{10, []string{"1", "2", "3"}, false, []string{"4", "5", "6"}},
+			want{[]string{"1", "2", "3", "4", "5", "6"}},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(t.Context())
+			defer cancel()
+
+			l, _ := test.NewNullLogger()
+
+			p := NewProducer(ctx, "name", tc.on.channelSize, l)
+
+			ch := p.Produce()
+
+			for _, value := range tc.on.addValuesBeforeCtxDone {
+				p.Add(value)
+			}
+
+			if tc.on.cancelCtx {
+				cancel()
+				time.Sleep(time.Millisecond * 100)
+			}
+
+			for _, value := range tc.on.addValuesAfterCtxDone {
+				p.Add(value)
+			}
+
+			values := make([]string, 0)
+			timeout := time.After(2 * time.Second)
+
+		loop:
+			for {
+				select {
+				case value, ok := <-ch:
+					if !ok {
+						break loop
+					}
+					values = append(values, value)
+				case <-timeout:
+					break loop
+				}
+			}
+
+			assert.Equal(t, tc.want.values, values)
+		})
+	}
+}
+
+//nolint:gocognit
+func TestProducer_Schedule(t *testing.T) {
+	type ctxDone struct {
+		beforeCtxDone time.Duration
+		cancelCtx     bool
+		afterCtxDone  time.Duration
+	}
+	type scheduleData struct {
+		value  string
+		delay  time.Duration
+		cancel bool
+	}
+	type on struct {
+		channelSize      uint
+		addBeforeCtxDone []scheduleData
+		ctxDone          ctxDone
+		addAfterCtxDone  []scheduleData
+		waiting          time.Duration
+	}
+	type want struct {
+		values []string
+	}
+	testCases := []struct {
+		name string
+		on   on
+		want want
+	}{
+		{
+			"ctx Done",
+			on{
+				10,
+				[]scheduleData{
+					{"1", time.Millisecond * 100, false},
+					{"2", time.Millisecond * 200, false},
+					{"3", time.Millisecond * 300, false},
+				},
+				ctxDone{
+					time.Millisecond * 150,
+					true,
+					time.Millisecond * 100,
+				},
+				[]scheduleData{
+					{"4", time.Millisecond * 110, false},
+					{"5", time.Millisecond * 210, false},
+					{"6", time.Millisecond * 310, false},
+					{"7", time.Second * 2, false},
+				},
+				time.Second * 1,
+			},
+			want{[]string{"1"}},
+		},
+		{
+			"ctx not Done",
+			on{
+				10,
+				[]scheduleData{
+					{"1", time.Millisecond * 100, false},
+					{"2", time.Millisecond * 200, false},
+					{"3", time.Millisecond * 300, false},
+				},
+				ctxDone{},
+				[]scheduleData{
+					{"4", time.Millisecond * 110, false},
+					{"5", time.Millisecond * 210, false},
+					{"6", time.Millisecond * 310, false},
+					{"7", time.Second * 2, false},
+				},
+				time.Second * 1,
+			},
+			want{[]string{"1", "4", "2", "5", "3", "6"}},
+		},
+		{
+			"cancel schedule",
+			on{
+				10,
+				[]scheduleData{
+					{"1", time.Millisecond * 100, true},
+					{"2", time.Millisecond * 200, false},
+					{"3", time.Millisecond * 300, true},
+				},
+				ctxDone{},
+				[]scheduleData{
+					{"4", time.Millisecond * 110, false},
+					{"5", time.Millisecond * 210, true},
+					{"6", time.Millisecond * 310, false},
+					{"7", time.Second * 2, false},
+					{"8", time.Second * 2, true},
+				},
+				time.Second * 1,
+			},
+			want{[]string{"4", "2", "6"}},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(t.Context())
+			defer cancel()
+			l, _ := test.NewNullLogger()
+
+			p := NewProducer(ctx, "name", tc.on.channelSize, l)
+			ch := p.Produce()
+
+			for _, schedule := range tc.on.addBeforeCtxDone {
+				scheduleCancel := p.Schedule(schedule.value, schedule.delay)
+				if schedule.cancel {
+					scheduleCancel()
+				}
+			}
+
+			if tc.on.ctxDone.cancelCtx {
+				time.Sleep(tc.on.ctxDone.beforeCtxDone)
+				cancel()
+				time.Sleep(tc.on.ctxDone.afterCtxDone)
+			}
+
+			for _, schedule := range tc.on.addAfterCtxDone {
+				scheduleCancel := p.Schedule(schedule.value, schedule.delay)
+				if schedule.cancel {
+					scheduleCancel()
+				}
+			}
+
+			values := make([]string, 0)
+			timeout := time.After(tc.on.waiting)
+
+		loop:
+			for {
+				select {
+				case value, ok := <-ch:
+					if !ok {
+						break loop
+					}
+					values = append(values, value)
+				case <-timeout:
+					break loop
+				}
+			}
+
+			assert.Equal(t, tc.want.values, values)
+		})
+	}
+}
