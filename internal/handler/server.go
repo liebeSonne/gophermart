632938916bb56fe65914ed394000e54861849afc
liebeSonne <>
2026-05-08 17:48:42 +0300

    v1 - исправил заголовок в методе получения баланса пользователя

--- internal/handler/server.go
+++ internal/handler/server.go
@@ -223,8 +223,6 @@ func (s *Server) GetUserOrders(w http.ResponseWriter, r *http.Request) {
 func (s *Server) GetUserBalance(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	w.Header().Set("Content-Type", "application/json")
-
 	userID, ok := auth.GetUserIDFromContext(ctx)
 	if !ok {
 		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
@@ -254,6 +252,7 @@ func (s *Server) GetUserBalance(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
 
